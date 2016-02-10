2b63e10714c797fc1b932819d24ed140984874c7
shishir <sk3432@drexel.edu>
2016-02-10 15:18:52 -0500

    added checkin

--- app.js
+++ app.js
@@ -7,11 +7,29 @@ var io = require('socket.io')(http);
 
 
 app.get('/', function(req, res) {
- res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
+var numUsers = 0;
+io.on('connection', function(socket) {
+    var addedUser = false;
+    console.log('a user connected');
+    socket.on('add user', function(username) {
+        if (addedUser) return;
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        console.log('useradded ' + username);
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
 });
 
 http.listen(port, function() {
--- public/main.js
+++ public/main.js
@@ -1,7 +1,49 @@
 (function() {
 
+    //TODO: put this to a seperate file
     var socket = io();
-    
+     var $loginPage = $('.login.page');
+    var $usernameInput = $('.usernameInput');
+    var $window = $(window);
+    var $currentInput = $usernameInput.focus();
+    var username;
+    $window.keydown(function(event) {
+        // Auto-focus the current input when a key is typed
+        if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+            $currentInput.focus();
+        }
+        // When the client hits ENTER on their keyboard
+        if (event.which === 13) {
+            if (username) {
+                sendMessage();
+                socket.emit('stop typing');
+                typing = false;
+            } else {
+                setUsername();
+            }
+        }
+    });
+
+    function setUsername() {
+        username = cleanInput($usernameInput.val().trim());
+
+        // If the username is valid
+        if (username) {
+            $loginPage.fadeOut();
+            //$chatPage.show();
+            $loginPage.off('click');
+            //$currentInput = $inputMessage.focus();
+
+            // Tell the server your username
+            socket.emit('add user', username);
+        }
+    }
+
+    // Prevents input from having injected markup
+    function cleanInput(input) {
+        return $('<div/>').text(input).text();
+    }
+
     var board,
         game = new Chess();
 
