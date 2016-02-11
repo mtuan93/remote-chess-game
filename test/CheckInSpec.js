0e12cd572c464f5651a06ea5fe56cd8c027b2431
Tuan <mtuan93@gmail.com>
2016-02-11 12:39:51 -0500

    complete the first test

--- test/CheckInSpec.js
+++ test/CheckInSpec.js
@@ -2,17 +2,14 @@ var should = require('should');
 var io = require('socket.io-client'),
     server = require('../app');
 
-var socketURL = 'http://localhost:3000';
+var socketURL = 'http://localhost:3000/';
 
 var options = {
     transports: ['websocket'],
     'force new connection': true
 };
 
-var user1 = {
-    userId: 1,
-    games: {}
-};
+var userName = "Tuan";
 
 describe("Chess Server", function() {
     /* Test 1 - A Single User */
@@ -20,12 +17,13 @@ describe("Chess Server", function() {
         var client = io.connect(socketURL, options);
 
         client.on('connect', function(data) {
-            client.emit('login', user1);
+            client.emit('login', userName);
         });
 
-        client.on('login', function(userId) {
-            userId.should.be.type('string');
-            userId.should.equal(user1.name + " has joined.");
+        client.on('login', function(user) {
+            user.should.be.type('object');
+            user.users.should.be.instanceof(Array);
+            user.games.should.be.instanceof(Array);
             /* If this client doesn't disconnect it will interfere 
       with the next test */
             client.disconnect();
